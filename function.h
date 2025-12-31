68d127ee8f008c0f5804227e4f137f033628e2a4
Dineth Lochana Mallawarachchi <lochandineth@gmail.com>
2025-12-31 16:27:50 +0530

    Revert "function.h optimizations for SmallerC"
    This reverts commit 42297b0dd812042b4f10bbf6969c131c373a8ca6.
    
    Revert Change

--- function.h
+++ function.h
@@ -65,86 +65,52 @@ float linearDeriv(float linearInput) {
 }
 
 void sigmoid(Matrix* input) {
-    size_t total = input->rows * input->cols;
-    size_t i;
-    if (input->stride == input->cols) {
-        for (i = 0; i < total; i++) {
-            input->data[i] = 1.0f / (1.0f + expf(-input->data[i]));
-        }
-    } else {
-        size_t r, c;
-        float* row;
-        for (r = 0; r < input->rows; r++) {
-            row = input->data + r * input->stride;
-            for (c = 0; c < input->cols; c++) {
-                row[c] = 1.0f / (1.0f + expf(-row[c]));
-            }
+    size_t i, j;
+    for (i = 0; i < input->rows; i++) {
+        for (j = 0; j < input->cols; j++) {
+            setMatrix(input, i, j, sigmoidFunc(getMatrix(input, i, j)));
         }
     }
 }
 
 void relu(Matrix* input) {
-    size_t total = input->rows * input->cols;
-    size_t i;
-    float val;
-    if (input->stride == input->cols) {
-        for (i = 0; i < total; i++) {
-            val = input->data[i];
-            input->data[i] = val > 0.0f ? val : 0.0f;
-        }
-    } else {
-        size_t r, c;
-        float* row;
-        for (r = 0; r < input->rows; r++) {
-            row = input->data + r * input->stride;
-            for (c = 0; c < input->cols; c++) {
-                val = row[c];
-                row[c] = val > 0.0f ? val : 0.0f;
-            }
+    size_t i, j;
+    for (i = 0; i < input->rows; i++) {
+        for (j = 0; j < input->cols; j++) {
+            setMatrix(input, i, j, reluFunc(getMatrix(input, i, j)));
         }
     }
 }
 
 void tanH(Matrix* input) {
-    size_t total = input->rows * input->cols;
-    size_t i;
-    if (input->stride == input->cols) {
-        for (i = 0; i < total; i++) {
-            input->data[i] = tanhf(input->data[i]);
-        }
-    } else {
-        size_t r, c;
-        float* row;
-        for (r = 0; r < input->rows; r++) {
-            row = input->data + r * input->stride;
-            for (c = 0; c < input->cols; c++) {
-                row[c] = tanhf(row[c]);
-            }
+    size_t i, j;
+    for (i = 0; i < input->rows; i++) {
+        for (j = 0; j < input->cols; j++) {
+            setMatrix(input, i, j, tanHFunc(getMatrix(input, i, j)));
         }
     }
 }
 
 void softmax(Matrix* input) {
     size_t i, j;
-    float maxVal, summed, invSum;
-    float* row;
     for (i = 0; i < input->rows; i++) {
-        row = input->data + i * input->stride;
-        /* Find max for numerical stability */
-        maxVal = row[0];
+        // Find max for numerical stability
+        float maxVal = getMatrix(input, i, 0);
         for (j = 1; j < input->cols; j++) {
-            if (row[j] > maxVal) maxVal = row[j];
+            float val = getMatrix(input, i, j);
+            if (val > maxVal) maxVal = val;
         }
-        /* Compute exp and sum */
-        summed = 0.0f;
+        
+        float summed = 0.0f;
         for (j = 0; j < input->cols; j++) {
-            row[j] = expf(row[j] - maxVal);
-            summed = summed + row[j];
+            float expVal = expf(getMatrix(input, i, j) - maxVal);
+            setMatrix(input, i, j, expVal);
+            summed = summed + expVal;
         }
-        /* Normalize */
-        invSum = 1.0f / summed;
+        
+        float invSum = 1.0f / summed;
         for (j = 0; j < input->cols; j++) {
-            row[j] = row[j] * invSum;
+            setMatrix(input, i, j, getMatrix(input, i, j) * invSum);
         }
     }
 }
